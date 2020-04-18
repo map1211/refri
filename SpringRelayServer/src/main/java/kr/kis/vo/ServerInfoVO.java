0d57949461b63715fb0829b37f3d30943422dee5
Jeong.Bongsu <jbongsu@gmail.com>
2020-04-18 18:24:48 +0900

    vo 추가 - 우선호

---
+++ SpringRelayServer/src/main/java/kr/kis/vo/ServerInfoVO.java
@@ -0,0 +1,12 @@
+package kr.kis.vo;
+
+public class ServerInfoVO {
+
+	public String relayServerType;
+	public String relayServerIp;
+	public int relayServerPort;
+	public String relayServerEncodeType;
+	public int relayServerSocketTimeout;
+	public int relayServerThreadNum;
+
+}
